ee7c619e0370032d36d18872fdf7c3122c925535
buikevin96 <buikevin96@gmail.com>
2018-08-16 11:51:39 -0400

    Started building the view for the API

--- rest/restapp/models.py
+++ rest/restapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# Database structure for Task Model
+class Task(models.Model):
+    task_name = models.CharField(max_length = 200) # Name of Task
+    task_desc = models.CharField(max_length = 200) # Task Description
+    date_created = models.DateTimeField(auto_now = True)# Date task was added
+    
--- rest/restapp/views.py
+++ rest/restapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets # Import rest framework viewsets
+from .serializers import TaskSerializers # Import taskSerializers from serializers.py
+from .models import Task # Import Task from models.py
 
 # Create your views here.
+
+# Define view for particular API
+class TaskViewSet(viewsets.ModelViewSet): # Inherits from 
+
+    # Extract data from the task model
+    queryset = Task.objects.all().order_by('-date_created')
+    serializer_class = TaskSerializers # Class to use with the view
